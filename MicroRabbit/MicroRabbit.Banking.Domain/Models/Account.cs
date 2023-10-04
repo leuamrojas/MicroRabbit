4385e3728f8cb0fb2446d907cc2bb865e531cc1b
Manuel Rojas <leuamrojas@hotmail.com>
2023-10-04 13:55:37 -0600

    Partially completed banking microservice

---
+++ MicroRabbit/MicroRabbit.Banking.Domain/Models/Account.cs
@@ -0,0 +1,9 @@
+﻿namespace MicroRabbit.Banking.Domain.Models
+{
+    public class Account
+    {
+        public int AccountId { get; set; }
+        public string AccountType { get; set; }
+        public decimal AccountBalance { get; set; }
+    }
+}
